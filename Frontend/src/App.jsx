4d2859d2972eae30311dd2a5a230799bc096c666
Purple-Snake <titas.sniuksta@gmail.com>
2024-04-05 12:57:10 +0300

    added axios and admin login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom"
 import MainPage from './components/client/MainPage'
+import AdminLogin from "./components/admin/AdminLogin"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
      <Routes>
         <Route path="/" element={<MainPage/>} />
+        <Route path="/admin" element={<AdminLogin/>} />
      </Routes>
     </>
   )
