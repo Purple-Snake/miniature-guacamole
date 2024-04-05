3300c9da41702812a2faefd31c029f05a69f5590
Purple-Snake <titas.sniuksta@gmail.com>
2024-04-05 13:08:31 +0300

    Fixed the images

--- Frontend/src/components/client/Card.jsx
+++ Frontend/src/components/client/Card.jsx
@@ -4,7 +4,7 @@ function Card({ item }) {
   return (
     <>
       <div key={item.id}>
-        <img src={item.itemPicture} alt={item.ItemName} />
+        <img src={item.itemPicture} alt={item.ItemName} style={{width:100}}/>
         <div>{item.itemName}</div>
         <div>{item.itemAmount}</div>
       </div>
